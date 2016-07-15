fbcf9aaa36325de6d71b94634224375bd2f7d0e7
Lenyn Alcántara <nightzpy@gmail.com>
2016-07-15 06:11:07 -0300

    Agregada solución ejercicios senior en el mismo .docx

--- Clases/Test.php
+++ Clases/Test.php
@@ -4,15 +4,13 @@ require_once ('Usuario.php');
 try {
 	
 	$usuario = new Usuario;
-/*
 	$usuario->codigoUsuario = 20;
 	$usuario->usuario = 'Lenyn';
 	$usuario->clave = md5('123456');
 	$usuario->edad = 5; 
 	$usuario->save();
-	*/
 
-	$usuario->findBy('codigo_usuario', 20, 'usuarios');
+	$usuario->findBy('codigo_usuario', 10, 'usuarios');
 	$usuario->delete();
 } catch (ValidationNotNullException $e) {
     $e->showMessage();
