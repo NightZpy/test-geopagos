ff4ba04743c7762673f4b6e95f0640a64899d7dc
Lenyn Alcántara <nightzpy@gmail.com>
2016-08-24 07:50:04 -0300

    Bases para solucion a test de figuras geométricas

---
+++ Factory/iFigura.php
@@ -0,0 +1,10 @@
+<?php
+
+interface iFigura
+{
+    public function getSuperficie();
+    public function getDiametro();
+    public function getBase();
+    public function getAltura();    
+    public function getTipo();    
+}
